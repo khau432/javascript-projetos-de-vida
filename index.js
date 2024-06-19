6823d36dd4d22b62975a65ce6e33bc6372df77e4
Khaua <111583051+khau432@users.noreply.github.com>
2024-06-19 10:03:54 -0300

    mlçkn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
-}
+
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
